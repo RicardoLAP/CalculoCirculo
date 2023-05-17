d4ec91db0b99d337bbe862cb31e5f4cf37ea0664
Ricardo Luiz A. Pereira <manayba@gmail.com>
2023-05-17 19:01:06 -0300

    1.1

--- README.md
+++ README.md
@@ -4,12 +4,18 @@
 
 =======
 Atividade
-(1)Crie uma função que calcule a área de um cícrculo com base no valor do raio informado pelo usuário.
+(1) Crie uma função que calcule a área de um cícrculo com base no valor do raio informado pelo usuário.
 (2) Crie uma função que calcule e mostre o valor da hipotenusa de um triangulo retângulo com base nos valores dos catetos informados pelo usuário.
 (3) Crie uma função que converta um valor em reais para doláres.
 (4) Crie botões na página principal para chmar as funções. Atualize seu repositório Github.
+(5) Calcular a média aritimética entre 3 notas de um aluno.
+(6) Obter as 2 raizes de uma equação do 2° grau a partir dos valores de a, b e c.
+(7) Converter uma temperatura em graus celsius para graus fahrenheit.
+(bhaskara)
 
 Hipotenusa: h² = co² + ca²
 Círculo: pi r²
-
+x1 = - b + raiz_quadrada(b²-4ac) / 2a
+x1 = - b - raiz_quadrada(b²-4ac) / 2a
+fahrenheit = (celsius * 1.8) + 32
 > > > > > > > Stashed changes
--- js/script.js
+++ js/script.js
@@ -17,3 +17,53 @@ function converterParaDolar() {
     valorDolar = valorReal / cotacaoDolar
     document.getElementById("resultado").innerHTML = "O valor em dólar é: $" + valorDolar.toFixed(2)
 }
+function calcularMedia() {
+    nota1 = parseFloat(document.getElementById("nota1").value);
+    nota2 = parseFloat(document.getElementById("nota2").value);
+    nota3 = parseFloat(document.getElementById("nota3").value);
+    media = (nota1 + nota2 + nota3) / 3;
+    document.getElementById("resultado").innerHTML = "A média é: " + media.toFixed(2);
+}
+
+function calcularRaizes() {
+    var a = parseFloat(prompt("Digite o valor de a na equação:"));
+    var b = parseFloat(prompt("Digite o valor de b na equação:"));
+    var c = parseFloat(prompt("Digite o valor de c na equação:"));
+    var delta = Math.pow(b, 2) - 4 * a * c;
+    
+    if (delta > 0) {
+      var raiz1 = (-b + Math.sqrt(delta)) / (2 * a);
+      var raiz2 = (-b - Math.sqrt(delta)) / (2 * a);
+    document.getElementById("resultado").innerHTML = "As raízes da equação são: " + raiz1.toFixed(2) + " e " + raiz2.toFixed(2);
+    } else if (delta === 0) {
+      var raiz = -b / (2 * a);
+    document.getElementById("resultado").innerHTML = "A equação possui uma raiz dupla: " + raiz.toFixed(2);
+} else {
+document.getElementById("resultado").innerHTML = "A equação não possui raízes reais.";
+}
+}
+
+function calcularRaizes() {
+    var a = parseFloat(document.getElementById("valorA").value);
+    var b = parseFloat(document.getElementById("valorB").value);
+    var c = parseFloat(document.getElementById("valorC").value);
+
+    var delta = Math.pow(b, 2) - (4 * a * c);
+
+    if (delta < 0) {
+        document.getElementById("resultado").innerHTML = "A equação não possui raízes reais.";
+    } else if (delta == 0) {
+        var x = -b / (2 * a);
+        document.getElementById("resultado").innerHTML = "A equação possui uma raiz real: x = " + x.toFixed(2);
+    } else {
+        var x1 = (-b + Math.sqrt(delta)) / (2 * a);
+        var x2 = (-b - Math.sqrt(delta)) / (2 * a);
+        document.getElementById("resultado").innerHTML = "As raízes da equação são: x1 = " + x1.toFixed(2) + ", x2 = " + x2.toFixed(2);
+    }
+}
+
+function converterTemperatura() {
+    var celsius = parseFloat(document.getElementById("celsius").value);
+    var fahrenheit = (celsius * 1.8) + 32;
+    document.getElementById("resultado").innerHTML = "A temperatura em Fahrenheit é: " + fahrenheit.toFixed(2);
+}
