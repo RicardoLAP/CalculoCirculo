0fed637bcbad0285b8fb9bb8ea2b29c919f7e771
Ricardo Luiz A. Pereira <manayba@gmail.com>
2023-05-23 15:14:11 -0300

    2.0

--- js/script.js
+++ js/script.js
@@ -1,69 +1,68 @@
 function calcularAreaCirculo() {
-    raio = parseFloat(prompt("Digite o valor do raio do círculo:"));
-    area = Math.PI * Math.pow(raio, 2)
-    document.getElementById("resultado").innerHTML = "A área do círculo é: " + area.toFixed(2)
+    var raio = parseFloat(document.getElementById("raioInput").value);
+    var area = Math.PI * raio * raio;
+    document.getElementById("areaCirculoOutput").innerHTML = "Área do Círculo: " + area.toFixed(2);
+}
+
+function calcularAreaRetangulo() {
+    var base = parseFloat(document.getElementById("baseInput").value);
+    var altura = parseFloat(document.getElementById("alturaInput").value);
+    var area = base * altura;
+    document.getElementById("areaRetanguloOutput").innerHTML = "Área do Retângulo: " + area.toFixed(2);
+}
+
+function calcularAreaTriangulo() {
+    var base = parseFloat(document.getElementById("baseTrianguloInput").value);
+    var altura = parseFloat(document.getElementById("alturaTrianguloInput").value);
+    var area = (base * altura) / 2;
+    document.getElementById("areaTrianguloOutput").innerHTML = "Área do Triângulo: " + area.toFixed(2);
 }
 
 function calcularHipotenusa() {
-    cateto1 = parseFloat(prompt("Digite o valor do primeiro cateto do triângulo retângulo:"))
-    cateto2 = parseFloat(prompt("Digite o valor do segundo cateto do triângulo retângulo:"))
-    hipotenusa = Math.sqrt(Math.pow(cateto1, 2) + Math.pow(cateto2, 2))
-    document.getElementById("resultado").innerHTML = "A hipotenusa do triângulo retângulo é: " + hipotenusa.toFixed(2)
+    var cateto1 = parseFloat(document.getElementById("cateto1Input").value);
+    var cateto2 = parseFloat(document.getElementById("cateto2Input").value);
+    var hipotenusa = Math.sqrt(cateto1 * cateto1 + cateto2 * cateto2);
+    document.getElementById("hipotenusaOutput").innerHTML = "Hipotenusa: " + hipotenusa.toFixed(2);
 }
 
-function converterParaDolar() {
-    valorReal = parseFloat(prompt("Digite o valor em reais:"))
-    cotacaoDolar = 4.93; // Exemplo de cotação do dólar
-    valorDolar = valorReal / cotacaoDolar
-    document.getElementById("resultado").innerHTML = "O valor em dólar é: $" + valorDolar.toFixed(2)
-}
-function calcularMedia() {
-    nota1 = parseFloat(document.getElementById("nota1").value);
-    nota2 = parseFloat(document.getElementById("nota2").value);
-    nota3 = parseFloat(document.getElementById("nota3").value);
-    media = (nota1 + nota2 + nota3) / 3;
-    document.getElementById("resultado").innerHTML = "A média é: " + media.toFixed(2);
-}
+function calcularBhaskara() {
+    var a = parseFloat(document.getElementById("aInput").value);
+    var b = parseFloat(document.getElementById("bInput").value);
+    var c = parseFloat(document.getElementById("cInput").value);
 
-function calcularRaizes() {
-    var a = parseFloat(prompt("Digite o valor de a na equação:"));
-    var b = parseFloat(prompt("Digite o valor de b na equação:"));
-    var c = parseFloat(prompt("Digite o valor de c na equação:"));
-    var delta = Math.pow(b, 2) - 4 * a * c;
-    
-    if (delta > 0) {
-      var raiz1 = (-b + Math.sqrt(delta)) / (2 * a);
-      var raiz2 = (-b - Math.sqrt(delta)) / (2 * a);
-    document.getElementById("resultado").innerHTML = "As raízes da equação são: " + raiz1.toFixed(2) + " e " + raiz2.toFixed(2);
-    } else if (delta === 0) {
-      var raiz = -b / (2 * a);
-    document.getElementById("resultado").innerHTML = "A equação possui uma raiz dupla: " + raiz.toFixed(2);
-} else {
-document.getElementById("resultado").innerHTML = "A equação não possui raízes reais.";
-}
-}
+    var delta = b * b - 4 * a * c;
 
-function calcularRaizes() {
-    var a = parseFloat(document.getElementById("valorA").value);
-    var b = parseFloat(document.getElementById("valorB").value);
-    var c = parseFloat(document.getElementById("valorC").value);
-
-    var delta = Math.pow(b, 2) - (4 * a * c);
-
-    if (delta < 0) {
-        document.getElementById("resultado").innerHTML = "A equação não possui raízes reais.";
-    } else if (delta == 0) {
-        var x = -b / (2 * a);
-        document.getElementById("resultado").innerHTML = "A equação possui uma raiz real: x = " + x.toFixed(2);
-    } else {
+    if (delta >= 0) {
         var x1 = (-b + Math.sqrt(delta)) / (2 * a);
         var x2 = (-b - Math.sqrt(delta)) / (2 * a);
-        document.getElementById("resultado").innerHTML = "As raízes da equação são: x1 = " + x1.toFixed(2) + ", x2 = " + x2.toFixed(2);
+        document.getElementById("bhaskaraOutput").innerHTML = "Soluções: x1 = " + x1.toFixed(2) + ", x2 = " + x2.toFixed(2);
+    } else {
+        document.getElementById("bhaskaraOutput").innerHTML = "Não existem soluções reais.";
     }
 }
 
+function converterMoedas() {
+    var reais = parseFloat(document.getElementById("reaisInput").value);
+    var dolares = reais / 4.9; // Considerando a cotação do dólar em 5.5 reais
+    document.getElementById("moedasOutput").innerHTML = "Valor em Dólares: " + dolares.toFixed(2);
+}
+
 function converterTemperatura() {
-    var celsius = parseFloat(document.getElementById("celsius").value);
-    var fahrenheit = (celsius * 1.8) + 32;
-    document.getElementById("resultado").innerHTML = "A temperatura em Fahrenheit é: " + fahrenheit.toFixed(2);
+    var celsius = parseFloat(document.getElementById("celsiusInput").value);
+    var fahrenheit = (celsius * 9 / 5) + 32;
+    document.getElementById("temperaturaOutput").innerHTML = "Temperatura em Fahrenheit: " + fahrenheit.toFixed(2);
+}
+
+function converterVelocidade() {
+    var kmh = parseFloat(document.getElementById("kmhInput").value);
+    var ms = kmh / 3.6;
+    document.getElementById("velocidadeOutput").innerHTML = "Velocidade em M/S: " + ms.toFixed(2);
+}
+
+function calcularMedia() {
+    var nota1 = parseFloat(document.getElementById("nota1Input").value);
+    var nota2 = parseFloat(document.getElementById("nota2Input").value);
+    var nota3 = parseFloat(document.getElementById("nota3Input").value);
+    var media = (nota1 + nota2 + nota3) / 3;
+    document.getElementById("mediaOutput").innerHTML = "Média: " + media.toFixed(2);
 }
